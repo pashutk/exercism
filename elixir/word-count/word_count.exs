ff206b0cc86810885f729f912c331ee0457d791e
pavelglushkov <pashutk@joom.it>
2019-02-24 20:32:58 +0300

    Solve elixir exercise

---
+++ elixir/word-count/word_count.exs
@@ -0,0 +1,21 @@
+defmodule Words do
+  @split_regex ~r/_+|[^\w\-]+/u
+
+  @doc """
+  Count the number of words in the sentence using @split_regex
+
+  _ symbol considered as whitespace.
+  - considered as non whitespace symbol.
+
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t()) :: map
+  def count(sentence) do
+    @split_regex
+    |> Regex.split(sentence, trim: true)
+    |> Enum.map(&String.downcase/1)
+    |> Enum.reduce(%{}, &reduce_occurrences/2)
+  end
+
+  defp reduce_occurrences(value, acc), do: Map.update(acc, value, 1, &(&1 + 1))
+end
